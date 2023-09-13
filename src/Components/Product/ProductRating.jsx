8f0c37661fde9f1979b987b6eb1257203420b242
Iasmin Corrêa Rebelo <iasmin_correa@hotmail.com>
2023-09-13 12:05:22 -0300

    ajustes product rating

--- src/Components/Product/ProductRating.jsx
+++ src/Components/Product/ProductRating.jsx
@@ -6,27 +6,17 @@ import starEmptyIcon from '../../Assets/star-empty.svg';
 const ProductRating = ({ rating }) => {
   const { rate, count } = rating;
 
-  const renderStar = (filled, key) => (
-    <img
-      key={key}
-      src={filled ? starFilledIcon : starEmptyIcon}
-      alt={filled ? 'Estrela Cheia' : 'Estrela Vazia'}
-    />
-  );
-
   const renderStars = () => {
-    const fullStars = Math.round(rate);
-    const emptyStars = 5 - fullStars;
     const stars = [];
-
-    for (let i = 0; i < fullStars; i++) {
-      stars.push(renderStar(true, `star-${i}`));
+    for (let i = 0; i < 5; i++) {
+      stars.push(
+        <img
+          key={`star-${i}`}
+          src={i < rate ? starFilledIcon : starEmptyIcon}
+          alt={i < rate ? 'Estrela Cheia' : 'Estrela Vazia'}
+        />,
+      );
     }
-
-    for (let i = 0; i < emptyStars; i++) {
-      stars.push(renderStar(false, `star-${fullStars + i}`));
-    }
-
     return stars;
   };
 
